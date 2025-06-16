adf0e0087936d760b2983f1e91fd287e1f254161
Yoake67 <fajarhidayat.9h@gmail.com>
2025-06-16 15:41:51 +0700

    mengaktifkan button menuju kisah nabi adam

--- app/src/main/java/com/uti/kisahnabi/KisahNabi.kt
+++ app/src/main/java/com/uti/kisahnabi/KisahNabi.kt
@@ -1,16 +1,26 @@
 package com.uti.kisahnabi
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class KisahNabi : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_kisah_nabi)
+
+        val buttonNabiAdam: Button = findViewById(R.id.BtnNabi1)
+
+        buttonNabiAdam.setOnClickListener {
+            val intent = Intent(this, NabiAdamActivity::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
