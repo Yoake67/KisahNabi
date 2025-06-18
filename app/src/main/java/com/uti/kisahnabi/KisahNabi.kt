1d2af18b54572000c4c85cda3cc4c2cf43904683
Yoake67 <fajarhidayat.9h@gmail.com>
2025-06-18 21:23:28 +0700

    membuat cerita kisah nabi nuh dan mengaktifkan button

--- app/src/main/java/com/uti/kisahnabi/KisahNabi.kt
+++ app/src/main/java/com/uti/kisahnabi/KisahNabi.kt
@@ -28,6 +28,13 @@ class KisahNabi : AppCompatActivity() {
             startActivity(intent)
         }
 
+        val buttonNabiNuh: Button = findViewById(R.id.BtnNabi3)
+
+        buttonNabiNuh.setOnClickListener {
+            val intent = Intent(this, NabiNuhActivity::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
