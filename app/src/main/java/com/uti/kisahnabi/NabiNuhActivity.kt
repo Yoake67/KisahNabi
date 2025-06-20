3be9c358615fa976bef6ad731f7190956592bafe
Yoake67 <fajarhidayat.9h@gmail.com>
2025-06-20 22:56:44 +0700

    mengaktifkan icon kembali ke kisah nabi dari halaman cerita para nabi

--- app/src/main/java/com/uti/kisahnabi/NabiNuhActivity.kt
+++ app/src/main/java/com/uti/kisahnabi/NabiNuhActivity.kt
@@ -14,6 +14,13 @@ class NabiNuhActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_nabi_nuh)
 
+        val buttonkeKembali: Button = findViewById(R.id.kembalikekisahnabi)
+
+        buttonkeKembali.setOnClickListener {
+            val intent = Intent(this, KisahNabi::class.java)
+            startActivity(intent)
+        }
+
         val buttonSebelumnya: Button = findViewById(R.id.sebelumnyake2)
 
         buttonSebelumnya.setOnClickListener {
