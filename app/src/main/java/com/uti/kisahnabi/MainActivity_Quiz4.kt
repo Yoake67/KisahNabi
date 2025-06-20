6d9f6a4ab6d5e006a414856e118a0b9dad7d35aa
fandev1 <fan200230@gmail.com>
2025-06-20 13:31:35 +0700

    tambah soal nomor 5 dan content

--- app/src/main/java/com/uti/kisahnabi/MainActivity_Quiz4.kt
+++ app/src/main/java/com/uti/kisahnabi/MainActivity_Quiz4.kt
@@ -1,20 +1,39 @@
 package com.uti.kisahnabi
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity_Quiz4 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main_quiz4)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val radioGroupSoal4 = findViewById<RadioGroup>(R.id.radioGroup4)
+        val buttonSubmit4 = findViewById<Button>(R.id.submit4)
+        val idJawabanBenarSoal4 = R.id.opsiB_soal4_jawaban // Nabi Ibrahim AS
+
+        buttonSubmit4.setOnClickListener {
+            val idJawabanTerpilih = radioGroupSoal4.checkedRadioButtonId
+
+            if (idJawabanTerpilih == -1) {
+                Toast.makeText(this, "Silakan pilih jawaban terlebih dahulu!", Toast.LENGTH_SHORT).show()
+            } else {
+                if (idJawabanTerpilih == idJawabanBenarSoal4) {
+                    Toast.makeText(this, "Jawaban Anda Benar!", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Jawaban Anda Salah.", Toast.LENGTH_SHORT).show()
+                }
+
+                // --- BAGIAN YANG DIUBAH ---
+                // Mengarahkan ke soal nomor 5
+                val intent = Intent(this, MainActivity_Quiz5::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
