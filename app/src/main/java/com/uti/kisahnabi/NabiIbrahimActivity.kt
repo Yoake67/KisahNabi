b5a1847b8be3df46f66f294ab1496c32ff258f12
Yoake67 <fajarhidayat.9h@gmail.com>
2025-06-22 14:19:36 +0700

    mengisi kisah di activity nabi ibrahim dan mengaktifkan button

--- app/src/main/java/com/uti/kisahnabi/NabiIbrahimActivity.kt
+++ app/src/main/java/com/uti/kisahnabi/NabiIbrahimActivity.kt
@@ -1,6 +1,8 @@
 package com.uti.kisahnabi
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,28 @@ class NabiIbrahimActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_nabi_ibrahim)
+
+        val buttonkeKembali: Button = findViewById(R.id.kembalikekisahnabi)
+
+        buttonkeKembali.setOnClickListener {
+            val intent = Intent(this, KisahNabi::class.java)
+            startActivity(intent)
+        }
+
+        val buttonSebelumnya: Button = findViewById(R.id.sebelumnyake5)
+
+        buttonSebelumnya.setOnClickListener {
+            val intent = Intent(this, NabiSalehActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonSelanjutnya: Button = findViewById(R.id.selanjutnyake7)
+
+        buttonSelanjutnya.setOnClickListener {
+            val intent = Intent(this, NabiIbrahimActivity::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
