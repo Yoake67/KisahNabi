17af55a697f5001a5627c5590d7013444021f76d
arya-fau <aryafauzan163@gmail.com>
2025-06-26 00:22:32 +0700

    membuat tombol kembali dari kisah rosul isa ke interface kisah rosul ulul azmi dan membuat tombol selanjutnya dari rosul isa ke rosul ibrahim

--- app/src/main/java/com/uti/kisahnabi/RosulIsaActivity.kt
+++ app/src/main/java/com/uti/kisahnabi/RosulIsaActivity.kt
@@ -1,6 +1,8 @@
 package com.uti.kisahnabi
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,28 @@ class RosulIsaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_rosul_isa)
+
+        val buttonkeKembali: Button = findViewById(R.id.kembalikekisahnabi)
+
+        buttonkeKembali.setOnClickListener {
+            val intent = Intent(this, KisahNabi::class.java)
+            startActivity(intent)
+        }
+
+        val buttonSelanjutnya: Button = findViewById(R.id.selanjutnyake2)
+
+        buttonSelanjutnya.setOnClickListener {
+            val intent = Intent(this, RosulIbrahimActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonKembali: Button = findViewById(R.id.kembalikisahnabi)
+
+        buttonKembali.setOnClickListener {
+            val intent = Intent(this, KisahNabi::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
