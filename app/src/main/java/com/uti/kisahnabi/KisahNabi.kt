f937963ca89e98c38dc431bcb1cfab14339a4368
Yoake67 <fajarhidayat.9h@gmail.com>
2025-06-26 23:43:49 +0700

    mengaktifkan button kisah nabi ke halaman kisah nabi zakaria

--- app/src/main/java/com/uti/kisahnabi/KisahNabi.kt
+++ app/src/main/java/com/uti/kisahnabi/KisahNabi.kt
@@ -168,6 +168,13 @@ class KisahNabi : AppCompatActivity() {
             startActivity(intent)
         }
 
+        val buttonNabiZakaria: Button = findViewById(R.id.BtnNabi22)
+
+        buttonNabiZakaria.setOnClickListener {
+            val intent = Intent(this, NabiZakariaActivity::class.java)
+            startActivity(intent)
+        }
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
